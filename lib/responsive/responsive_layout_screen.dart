b253c18e5c1423131cd42afb59ea76b3060d3a33
HEMANG SINGH <hemang.cs19@bmsce.ac.in>
2022-04-24 13:18:18 +0530

    Add files via upload

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+import 'package:highin_app/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget{
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout({
+    Key? key, required this.webScreenLayout, required this.mobileScreenLayout, }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return LayoutBuilder(
+      builder: (context, constraints){
+        if(constraints.maxWidth> webScreenSize){
+          return webScreenLayout;
+          //for web screen
+        }
+        return mobileScreenLayout;
+        //for mobile screen
+
+      },
+    );
+  }
+}
